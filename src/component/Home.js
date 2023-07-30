c355b4bcdf54e56be2cd329ec3636acf5b9a95cd
Om Kharche <omkharche321@gamil.com>
2023-07-30 21:41:38 +0530

    carousel length updated

--- src/component/Home.js
+++ src/component/Home.js
@@ -14,7 +14,7 @@ export default function Home() {
                     <img class="w-100 h-100" src="img/carousel-1.jpg" alt="Image" />
                     <div class="carousel-caption d-flex flex-column align-items-center justify-content-center p-5">
                         <div class="p-3" style={{maxWidth: '900px'}}>
-                            <h5 class="text-white text-uppercase">Best Badminton Club</h5>
+                           {/*  <h5 class="text-white text-uppercase">Best Badminton Club</h5> */}
                             <h1 class="display-2 text-white text-uppercase mb-md-4">Beyond Limits : Join Achievers Badminton Club!</h1>
                             <a href="" class="btn btn-primary py-md-3 px-md-5 me-3">Join Us</a>
                             <a href="" class="btn btn-light py-md-3 px-md-5">Contact Us</a>
